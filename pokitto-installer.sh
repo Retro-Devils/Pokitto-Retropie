e01f269fc55e6d79a3605ac8bcf7668c8a1cc7b6
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-10 13:18:05 -0700

    Update pokitto-installer.sh

--- pokitto-installer.sh
+++ pokitto-installer.sh
@@ -8,7 +8,7 @@ function pokitto-menu() {
     local choice
 
     while true; do
-        choice=$(dialog --backtitle "$BACKTITLE" --title "PIKITTO RETROPIE INSTALLER V1.00 " \
+        choice=$(dialog --backtitle "$BACKTITLE" --title "POKITTO RETROPIE INSTALLER V1.00 " \
             --ok-label Select --cancel-label Exit-Installer \
             --menu "PIKITTO RETROPIE INSTALLER" 25 50 30 \
             1 "Install Pokitto  " \
@@ -34,20 +34,41 @@ The development library can be found at the PokittoLib repository." 0 0
 }
 
 function install_pokitto() {
+echo "
+CHECKING IF POKITTO EMU IS INSTALLED
+-------------------------------------" 
+sleep 2
 if [ -d "/opt/retropie/emulators/pokitto" ]; then sudo rm -R /opt/retropie/emulators/pokitto; fi
 if [ -d "/opt/retropie/configs/pokitto" ]; then sudo rm -R /opt/retropie/configs/pokitto; fi
 if [ -f "/usr/local/bin/pokitto" ]; then sudo rm -R /usr/local/bin/pokitto; fi
+echo "
+CHECK COMPLETE
+-------------------------------------" 
+sleep 3
 sudo wget https://github.com/Retro-Devils/Pokitto-Retropie/raw/main/pokitto.zip -P /opt/retropie/emulators/
 sudo unzip /opt/retropie/emulators/pokitto.zip
-sudo mv -R $HOME/pokitto -P /opt/retropie/emulators
+sudo mv $HOME/pokitto /opt/retropie/emulators/
 sudo rm /opt/retropie/emulators/pokitto.zip
 sudo chmod -R 755 /opt/retropie/emulators/pokitto
 cd /opt/retropie/emulators/pokitto
 make
 sleep 1
 cd
+echo "
+Installing/Checking For Dependencies
+-------------------------------------" 
+sleep 1
+echo "Installing P7Zip"
+sudo apt-get install -y p7zip
+echo "Installing unzip"
+sudo apt-get install -y unzip
+echo "Installing LibSDl2-Net"
+sudo apt-get install -y libsdl2-net-dev
 if [ ! -d "$HOME/.qjoypad3" ]; then sudo apt-get install qjoypad; fi
-sudo apt-get install sdl1
+echo "
+-----------------------------
+Dependencies Install Finished
+-----------------------------"
 sudo mkdir /opt/retropie/configs/pokitto
 mkdir $HOME/RetroPie/roms/pokitto
 sudo wget https://raw.githubusercontent.com/Retro-Devils/Pokitto-Retropie/main/emulators.cfg -P /opt/retropie/configs/pokitto/
