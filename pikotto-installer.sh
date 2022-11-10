b74e29ce73c6cb9475bcc36ea3772b4faee203e3
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-10 01:09:38 -0600

    Update pikotto-installer.sh

--- pikotto-installer.sh
+++ pikotto-installer.sh
@@ -35,7 +35,7 @@ The development library can be found at the PokittoLib repository." 0 0
 
 function install_pikitto() {
 wget https://github.com/Retro-Devils/Pokitto-Retropie/blob/main/PokittoEmu-1.0.2.zip
-unzip PokittoEmu-1.0.2.zip
+unzip $HOME/PokittoEmu-1.0.2.zip
 rm $HOME/PokittoEmu-1.0.2.zip
 cd $HOME/PokittoEmu-1.0.2
 make
