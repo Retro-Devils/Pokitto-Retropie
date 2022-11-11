76920e3ffb82567f7e3163e8d5ad7d844c8e193a
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-11-11 07:48:57 -0700

    Update pokitto-installer.sh

--- pokitto-installer.sh
+++ pokitto-installer.sh
@@ -16,7 +16,7 @@ function pokitto-menu() {
             2>&1 > /dev/tty)
 
         case "$choice" in
-            1) install_pokitto  ;;
+            1) install-pokitto  ;;
             2) info        ;;
             -) no ;;
             *) break       ;;
@@ -33,27 +33,8 @@ An emulator for the Pokitto DIY handheld game system.
 The development library can be found at the PokittoLib repository." 0 0
 }
 
-function install_pokitto() {
-    local choice
 
-    while true; do
-        choice=$(dialog --backtitle "$BACKTITLE" --title "POKITTO RETROPIE INSTALLER V1.00 " \
-            --ok-label Select --cancel-label Exit-Installer \
-            --menu "DO YOU HAVE PIXEL DESKTOP INSTALLED?" 25 50 30 \
-            1 "YES Install Pokitto  " \
-            2 "NO/ UNSURE Install Both   " \
-            2>&1 > /dev/tty)
-
-        case "$choice" in
-            1) pixel-yes  ;;
-            2) pixel-no   ;;
-            -) no ;;
-            *) break       ;;
-        esac
-    done
-}
-
-function pixel-no() {
+function install-pokitto() {
 echo "
 CHECKING IF POKITTO EMU IS INSTALLED
 -------------------------------------" 
@@ -90,8 +71,12 @@ sudo apt-get install -y libsdl2-net-dev
 echo "Installing Window Manager Stuff"
 sudo apt-get install -y matchbox-window-manager
 apt install libsdl2-image-dev
-sudo $HOME/RetroPie-Setup//retropie_packages.sh b-em-pico-pi
-if [ ! -d "$HOME/.qjoypad3" ]; then sudo apt-get install qjoypad; fi
+sudo apt-get install qjoypad
+if [ ! -f /usr/bin/startx ]; then
+dialog  --sleep 1 --title "PIXEL DESKTOP NOT INSTALLED !! " --msgbox " 
+INSTALLING NOW" 0 0
+cd $HOME/RetroPie-Setup
+sudo ./retropie_packages.sh raspbiantools lxde
 echo "
 -----------------------------
 Dependencies Install Finished
@@ -107,59 +92,7 @@ sudo chmod -R 755 /opt/retropie/configs/pokitto/*
 sudo chmod -R 755 /opt/retropie/emulators/pokitto/*
 sudo chmod 755 /usr/local/bin/pokitto
 sleep 1
-dialog  --sleep 1 --title "INSTALL COMPLETE" --msgbox "
-- YOU WILL NEED TO MANUALLY EDIT ES-SYSTEMS.CFG
-- A FOLDER HAS BEEN MADE FOR YOUR GAMES
-THE FOLLOWING IS WHAT YOU NEED TO ADD TO ES-SYSTEMS.CFG
-  <system>
-    <name>pokitto</name>
-    <fullname>Pokitto</fullname>
-    <path>/home/pi/RetroPie/roms/pokitto</path>
-    <extension>.BIN .bin </extension>
-    <command>/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ pokitto %ROM%</command>
-    <platform>pokitto</platform>
-    <theme>pokitto</theme>
-  </system>" 0 0
-}
-
-
-function pixel-yes() {
-echo "
-CHECKING IF POKITTO EMU IS INSTALLED
--------------------------------------" 
-sleep 2
-if [ -d "/opt/retropie/emulators/pokitto" ]; then sudo rm -R /opt/retropie/emulators/pokitto; fi
-if [ -d "/opt/retropie/configs/pokitto" ]; then sudo rm -R /opt/retropie/configs/pokitto; fi
-if [ -f "/usr/local/bin/pokitto" ]; then sudo rm /usr/local/bin/pokitto; fi
-if [ -f "$HOME/.qoyjpad3/pokitto.lyt" ]; then sudo rm $HOME/.qoyjpad3/pokitto.lyt; fi
-if [ -f "$HOME/pokitto.zip" ]; then sudo rm $HOME/pokitto.zip; fi
-echo "
-CHECK COMPLETE
--------------------------------------" 
-sleep 3
-sudo wget https://github.com/Retro-Devils/Pokitto-Retropie/raw/main/pokitto.zip -P $HOME/
-sudo unzip $HOME/pokitto.zip
-rm $HOME/pokitto.zip
-cd $HOME/pokitto
-sudo make
-sleep 1
-cd
-sudo cp -R $HOME/pokitto /opt/retropie/emulators/
-sudo chmod -R 755 /opt/retropie/emulators/pokitto/*
-sudo rm -R $HOME/pokitto
-echo "
-Installing/Checking For Dependencies
--------------------------------------" 
-sleep 1
-echo "Installing P7Zip"
-sudo apt-get install -y p7zip
-echo "Installing unzip"
-sudo apt-get install -y unzip
-echo "Installing LibSDl2-Net"
-sudo apt-get install -y libsdl2-net-dev
-echo "Installing Window Manager Stuff"
-apt install libsdl2-image-dev
-if [ ! -d "$HOME/.qjoypad3" ]; then sudo apt-get install qjoypad; fi
+else
 echo "
 -----------------------------
 Dependencies Install Finished
@@ -175,6 +108,7 @@ sudo chmod -R 755 /opt/retropie/configs/pokitto/*
 sudo chmod -R 755 /opt/retropie/emulators/pokitto/*
 sudo chmod 755 /usr/local/bin/pokitto
 sleep 1
+fi
 dialog  --sleep 1 --title "INSTALL COMPLETE" --msgbox "
 - YOU WILL NEED TO MANUALLY EDIT ES-SYSTEMS.CFG
 - A FOLDER HAS BEEN MADE FOR YOUR GAMES
@@ -189,5 +123,6 @@ THE FOLLOWING IS WHAT YOU NEED TO ADD TO ES-SYSTEMS.CFG
     <theme>pokitto</theme>
   </system>" 0 0
 }
+
 
 pokitto-menu
